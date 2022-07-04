5067e3dcb35aff6b84fb157a808bae66314a70b3
Kozyrev AR <KozyrevAR@mpei.ru>
2022-07-04 06:09:05 +0300

    3.2.7 до конца

--- lab04.cpp
+++ lab04.cpp
@@ -98,15 +98,30 @@ void show_histogram_text(vector<size_t>& bins, size_t& bin_count, size_t& number
 }
 int main(int argc, char* argv[])
 {
-    if (argc > 1) 
+    curl_global_init(CURL_GLOBAL_ALL);
+    if (argc > 1)
     {
-        curl_global_init(CURL_GLOBAL_ALL);
+        CURL* curl = curl_easy_init();
+        if (curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            if (res != CURLE_OK)
+            {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+                exit(1);
+            }
+        }
+        return 0;
+    }
         string color = " ";
         const auto input = read_input(cin, true);
         const auto bins = make_histogram(input);
         show_histogram_svg(bins, color);
-        return 0;
-    }
+        
+    
 }
 
 
